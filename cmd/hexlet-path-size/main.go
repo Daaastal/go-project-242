0ca30ca91cbf1a5619d98d7f573276adba1319c2
Dmitry Sheberstov <dsheberstov@aq.ru>
2026-05-22 12:46:02 +0300

    Реализация вывода размера директории или файла

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"os"
+	"fmt"
+	"log"
 	"context"
+	"path/filepath"
 	"github.com/urfave/cli/v3"
 )
 
@@ -11,9 +14,47 @@ func main() {
 		Name:		"hexlet-path-size",
 		Usage:		"print size of a file or directory",
 		ArgsUsage:	"<path>",
+		Action: func(_ context.Context, cmd *cli.Command) error {
+			path := cmd.Args().Get(0)
+
+			size, err := GetPathSize(path, false, false, false)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%s\t%s", size, path)
+			return nil
+		},
 	}
 
-	os.Args = []string{"hexlet-path-size"}
-
 	_ = cmd.Run(context.Background(), os.Args)
+}
+
+func GetPathSize(path string, recursive, human, all bool) (string, error) {
+	var size int64
+	size = 0
+
+	fi, err := os.Lstat(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mode := fi.Mode()
+	if mode.IsDir() {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			fi, err := os.Lstat(filepath.Join(path, file.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			size += fi.Size()
+		}
+	} else {
+		size += fi.Size()
+	}
+	return fmt.Sprintf("%dB", size), nil
 }
