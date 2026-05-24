c48dfb050e10aef3ca5a54ee1a3e1be8a63148ba
Dmitry Sheberstov <dsheberstov@aq.ru>
2026-05-24 19:26:16 +0300

    Выполение 5 шага

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -14,10 +14,18 @@ func main() {
 		Name:		"hexlet-path-size",
 		Usage:		"print size of a file or directory",
 		ArgsUsage:	"<path>",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:	 "human",
+				Aliases: []string{"H"},
+				Usage:	 "human-readable sizes (auto-select unit)",
+			},
+		},
 		Action: func(_ context.Context, cmd *cli.Command) error {
 			path := cmd.Args().Get(0)
+			human := cmd.Bool("human")
 
-			size, err := GetPathSize(path, false, false, false)
+			size, err := GetPathSize(path, false, human, false)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -31,8 +39,7 @@ func main() {
 }
 
 func GetPathSize(path string, recursive, human, all bool) (string, error) {
-	var size int64
-	size = 0
+	size := int64(0)
 
 	fi, err := os.Lstat(path)
 	if err != nil {
@@ -55,6 +62,44 @@ func GetPathSize(path string, recursive, human, all bool) (string, error) {
 		}
 	} else {
 		size += fi.Size()
+	}
+
+	if human {
+		return convertHumanFormat(size)
+	}
+
+	return fmt.Sprintf("%dB", size), nil
+}
+
+func convertHumanFormat(size int64) (string, error) {
+	newSize := int64(0)
+	remains := int64(0)
+	count := 0
+
+	if size < 1024 {
+		return fmt.Sprintf("%dB", size), nil
+	}
+
+	for size != 0 {
+		newSize = size
+		remains = size % 1024
+		size /= 1024
+		count++;
+	}
+
+	switch count {
+	case 1:
+		return fmt.Sprintf("%d.%dKB", newSize, remains), nil
+	case 2:
+		return fmt.Sprintf("%d.%dMB", newSize, remains), nil
+	case 3:
+		return fmt.Sprintf("%d.%dGB", newSize, remains), nil
+	case 4:
+		return fmt.Sprintf("%d.%dTB", newSize, remains), nil
+	case 5:
+		return fmt.Sprintf("%d.%dPB", newSize, remains), nil
+	case 6:
+		return fmt.Sprintf("%d.%dEB", newSize, remains), nil
 	}
 	return fmt.Sprintf("%dB", size), nil
 }
